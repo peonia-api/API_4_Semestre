ee31948bcdddaf060dd93b482539413a0d31e7c5
laaridiniz <diniz.laari@gmail.com>
2023-03-31 11:36:59 -0300

    Inserção do formulário no modal

--- front/src/components/Modal.tsx
+++ front/src/components/Modal.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import editar from '../images/editar.png';
+import { Form } from 'react-bootstrap';
 
 function MyButton() {
     const [showModal, setShowModal] = useState(false);
@@ -25,17 +26,73 @@ function MyButton() {
 
             <Modal show={showModal} onHide={handleClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Modal Title</Modal.Title>
+                    <Modal.Title>Editar Solicitação</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <p>Modal content goes here.</p>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                        <Form.Label>Nome</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            autoFocus
+                        />
+                        </Form.Group>
+                        <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1">
+                        <Form.Label>E-mail</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            autoFocus
+                        />
+                        </Form.Group>
+                        <Form.Group 
+                        className="mb-3" 
+                        controlId="exampleForm.ControlInput1">
+                        <Form.Label>Telefone</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            autoFocus
+                        />
+                        </Form.Group>
+                        <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                        >
+                        <Form.Label>Título</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            autoFocus
+                        />
+                        </Form.Group>
+                        <Form.Group className="mb-3" 
+                        controlId="exampleForm.ControlInput1">
+                        <Form.Label>Tipo de Chamado</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder=""
+                            autoFocus
+                        />
+                        </Form.Group>
+                        <Form.Group
+                        className="mb-3"
+                        controlId="exampleForm.ControlTextarea1"
+                        >
+                        <Form.Label>Descrição</Form.Label>
+                        <Form.Control as="textarea" rows={20} />
+                        </Form.Group>
+                    </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
-                        Close
+                        Fechar
                     </Button>
                     <Button variant="primary" onClick={handleClose}>
-                        Save Changes
+                        Salvar Alterações
                     </Button>
                 </Modal.Footer>
             </Modal>
