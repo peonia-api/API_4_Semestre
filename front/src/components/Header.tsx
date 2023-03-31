20dffe3de870ae7f759ae4c837d1955af02503c3
Renan Vitor <renanv.f.m@hotmail.com>
2023-03-31 10:17:57 -0300

    Alt de nome da pag, retirada temporaria do Perfil

--- front/src/components/Header.tsx
+++ front/src/components/Header.tsx
@@ -1,20 +1,25 @@
-import { Link, useMatch, useResolvedPath } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import Images from '../images/logo_navbar.svg'
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Images from "../images/logo_navbar.svg";
 
 function Header() {
-
   return (
     <>
-      <Navbar collapseOnSelect fixed="top" expand="sm" variant="dark" className='position-fixed w-100 top-0 background-header'>
-        <Container className='mb-2'>
+      <Navbar
+        collapseOnSelect
+        fixed="top"
+        expand="sm"
+        variant="dark"
+        className="position-fixed w-100 top-0 background-header"
+      >
+        <Container className="mb-2">
           <Navbar.Brand to="/" as={CustomLink}>
-            <img 
-            src="https://uploads-ssl.webflow.com/60dcc4691817e11aa93685ab/636cfbef568f863947dd4951_logo-color.svg" 
-            alt="Logotipo de IONIC Health" 
-            className="d-inline-block align-top"
+            <img
+              src="https://uploads-ssl.webflow.com/60dcc4691817e11aa93685ab/636cfbef568f863947dd4951_logo-color.svg"
+              alt="Logotipo de IONIC Health"
+              className="d-inline-block align-top"
             />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav"></Navbar.Toggle>
@@ -22,27 +27,36 @@ function Header() {
             <Nav>
               <CustomLink to="/">SOLICITAÇÃO</CustomLink>
               <CustomLink to="/listagem">LISTAGEM</CustomLink>
-              <CustomLink to="/perfil">PERFIL</CustomLink>
+              {/* <CustomLink to="/perfil">PERFIL</CustomLink> */}
             </Nav>
-          </Navbar.Collapse>          
+          </Navbar.Collapse>
         </Container>
-      </Navbar>      
+      </Navbar>
     </>
-  )
-
+  );
 }
 
-function CustomLink({ to, children, ...props } : { to: string; children: string }) {
+function CustomLink({
+  to,
+  children,
+  ...props
+}: {
+  to: string;
+  children: string;
+}) {
+  const resolvedPath = useResolvedPath(to);
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true });
 
-  const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-  
   return (
-    <Nav.Link as={Link} to={to} className={"me-2" + (isActive ? " active" : "")} {...props}>
+    <Nav.Link
+      as={Link}
+      to={to}
+      className={"me-2" + (isActive ? " active" : "")}
+      {...props}
+    >
       {children}
     </Nav.Link>
-  )
-
+  );
 }
 
 export default Header;
