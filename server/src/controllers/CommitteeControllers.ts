865efa81b64e9f487097aa479607e30ac1fd12e7
Jennyads <jennycristina.1998@gmail.com>
2023-04-18 12:07:13 -0300

    adiciona rotas de atualização para cada item de avaliação do comitê

--- server/src/controllers/CommitteeControllers.ts
+++ server/src/controllers/CommitteeControllers.ts
@@ -18,11 +18,11 @@ class CommitteeController {
         const createCommittee = req.body
         const committeeRepository = AppDataSource.getRepository(Committee)
         const insertCommittee = new Committee();
-        insertCommittee.comiImpactCto = createCommittee.comiImpactCto
-        insertCommittee.comiImpactHp = createCommittee.comiImpactHp
-        insertCommittee.comiCostSquad = createCommittee.comiCostSquad
-        insertCommittee.comiRiskRt = createCommittee.comiRiskRt
-        insertCommittee.comiRiskCso = createCommittee.comiRiskCso
+        insertCommittee.comiImpactCto = null
+        insertCommittee.comiImpactHp = null
+        insertCommittee.comiCostSquad = null
+        insertCommittee.comiRiskRt = null
+        insertCommittee.comiRiskCso = null
         insertCommittee.call = createCommittee.call
   
     
@@ -31,21 +31,86 @@ class CommitteeController {
         return res.json(allCommittee)
     }
 
-    public async putCommittee (req: Request, res: Response) : Promise<Response> {
+    public async putCommitteeImpactCto (req: Request, res: Response) : Promise<Response> {
         const createCommittee = req.body
         const idCommittee:any = req.params.uuid
         const committeeRepository = AppDataSource.getRepository(Committee)
         const findCommittee = await committeeRepository.findOneBy({id: idCommittee})
         findCommittee.comiImpactCto = createCommittee.comiImpactCto
-        findCommittee.comiImpactHp = createCommittee.comiImpactHp
-        findCommittee.comiCostSquad = createCommittee.comiCostSquad
-        findCommittee.comiRiskRt = createCommittee.comiRiskRt
-        findCommittee.comiRiskCso = createCommittee.comiRiskCso
 
-    
         const allCommittee = await committeeRepository.save(findCommittee)
         return res.json(allCommittee)
     }
+    public async putCommitteeImpactHp (req: Request, res: Response) : Promise<Response> {
+        const createCommittee = req.body
+        const idCommittee:any = req.params.uuid
+        const committeeRepository = AppDataSource.getRepository(Committee)
+        const findCommittee = await committeeRepository.findOneBy({id: idCommittee})
+        findCommittee.comiImpactHp = createCommittee.comiImpactHp
+
+        const allCommittee = await committeeRepository.save(findCommittee)
+        return res.json(allCommittee)
+    }
+    public async putCommitCostSquad (req: Request, res: Response) : Promise<Response> {
+        const createCommittee = req.body
+        const idCommittee:any = req.params.uuid
+        const committeeRepository = AppDataSource.getRepository(Committee)
+        const findCommittee = await committeeRepository.findOneBy({id: idCommittee})
+        findCommittee.comiCostSquad = createCommittee.comiCostSquad
+
+        const allCommittee = await committeeRepository.save(findCommittee)
+        return res.json(allCommittee)
+    }
+    public async putCommitRikRt (req: Request, res: Response) : Promise<Response> {
+        const createCommittee = req.body
+        const idCommittee:any = req.params.uuid
+        const committeeRepository = AppDataSource.getRepository(Committee)
+        const findCommittee = await committeeRepository.findOneBy({id: idCommittee})
+        findCommittee.comiRiskRt = createCommittee.comiRiskRt
+
+        const allCommittee = await committeeRepository.save(findCommittee)
+        return res.json(allCommittee)
+    }
+    public async putCommitRiskCso (req: Request, res: Response) : Promise<Response> {
+        const createCommittee = req.body
+        const idCommittee:any = req.params.uuid
+        const committeeRepository = AppDataSource.getRepository(Committee)
+        const findCommittee = await committeeRepository.findOneBy({id: idCommittee})
+        findCommittee.comiRiskCso = createCommittee.comiRiskCso
+
+        const allCommittee = await committeeRepository.save(findCommittee)
+        return res.json(allCommittee)
+    }
+    public async getCommitteeFilter (req: Request, res: Response) : Promise<Response> {
+        const idCommittee:any = req.params.uuid
+        const committeeRepository = AppDataSource.getRepository(Committee)
+        const allCommittee = await committeeRepository.findOneBy({id: idCommittee})
+        const validate = {mensage:"Nota com alto risco, a feature deve ser arquivada!"}
+        const validate1 = {mensage:"Nota com baixo risco, a feature deve ser arquivada!"}
+        
+        if (allCommittee.comiRiskCso == 3) {
+            return res.json(validate)
+        }
+        else if (allCommittee.comiRiskRt == 3) {
+            return res.json(validate)
+        }
+        else if (allCommittee.comiImpactCto == 0) {
+            return res.json(validate1)
+        }
+        else if (allCommittee.comiImpactHp == 0) {
+            return res.json(validate1)
+        }
+        else if (allCommittee.comiImpactHp == 3) {
+            return res.json(validate)
+        }
+        else{
+            return res.json(allCommittee)
+
+        }
+
+        
+    }
+
 
 }
 export default new CommitteeController();
