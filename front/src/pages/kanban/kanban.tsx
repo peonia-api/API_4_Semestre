73bd85ba9b4a746e3c663ee58f09c463d471a504
mateushlsilva <mateushls01@gmail.com>
2023-05-29 19:07:30 -0300

    Atualiza kanban

--- front/src/pages/kanban/kanban.tsx
+++ front/src/pages/kanban/kanban.tsx
@@ -9,16 +9,7 @@ import { taskBody } from "../../utils/axiosPatch";
 
 function KanbanBoard() {
     const [data, setData] = useState(Object);
-
-    function createKanbanRemoteDatasource() {
-        return new DataManager({
-          url: 'https://ej2services.syncfusion.com/production/web-services/api/Kanban',
-          adaptor: new ODataAdaptor(),
-          crossDomain: true
-        });
-      }
-      const kanbanRemoteDatasource = createKanbanRemoteDatasource();
-
+    
       useEffect(() => {
         axios.get(URItask.PEGAR_TAKS)
            .then(async (res) => {
@@ -28,58 +19,6 @@ function KanbanBoard() {
 
      }, []);
 
-    console.log(data);
-    
-
-    function kanbanData(): Object[] {
-        return [
-            {
-                "Id": "Task 1",
-                "Title": "Task - 29001",
-                "Status": "Aprovada",
-                "Summary": "Analyze the new requirements gathered from the customer.",
-                "Priority": "Low",
-                "type": "feature",
-                "color": "#148eba"
-            },
-            {
-                "Id": "Task 2",
-                "Title": "Task - 29002",
-                "Status": "Fazendo",
-                "Summary": "Improve application performance",
-                "Priority": "Normal",
-                "type": "hotfix",
-                "color": "#9e1208"
-            },
-            {
-                "Id": "Task 3",
-                "Title": "Task - 3",
-                "Status": "Feito",
-                "Summary": "Fix the issues reported by the customer.",
-                "Priority": "Low",
-                "type": "hotfix",
-                "color": "#9e1208"
-            },
-            {
-                "Id": "Task 4",
-                "Title": "Task - 4",
-                "Status": "Fazendo",
-                "Summary": "Improve application performance",
-                "Priority": "Normal",
-                "type": "hotfix",
-                "color": "#9e1208"
-            },
-            {
-                "Id": "Task 5",
-                "Title": "Task - 5",
-                "Status": "Feito",
-                "Summary": "Fix the issues reported by the customer.",
-                "Priority": "Low",
-                "type": "hotfix",
-                "color": "#9e1208"
-            }
-        ];
-    }
 
     return (
         <>
