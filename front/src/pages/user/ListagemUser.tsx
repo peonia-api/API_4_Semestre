6fa8f53ac831ddf7da6bb557002c0e31743d08ec
Diego Batista <88885382+diiegobsilva@users.noreply.github.com>
2023-05-30 12:22:20 -0300

    seach User

--- front/src/pages/user/ListagemUser.tsx
+++ front/src/pages/user/ListagemUser.tsx
@@ -86,6 +86,17 @@ function ListagemUser() {
     setCurrentPage(data.selected);
   };
 
+  const [searchQuery, setSearchQuery] = useState("");
+  
+  const filteredData = data.filter(
+    (item) =>
+    item.userName.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    item.userEmail.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    item.userType.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    item.userPosition.toLowerCase().includes(searchQuery.toLowerCase())
+    
+  )
+
 
   return (
     <>
@@ -104,6 +115,13 @@ function ListagemUser() {
                     <path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z" />
                   </svg>
                 </button>
+                <input
+                  className="input-search"
+                  type="text"
+                  placeholder="Pesquisar"
+                  value={searchQuery}
+                  onChange={(event) => setSearchQuery(event.target.value)}
+                />
               </div>
               <Table bordered hover responsive>
                 <thead>
@@ -117,7 +135,7 @@ function ListagemUser() {
                 </thead>
 
                 <tbody>
-                  {data.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage).map((data) => {
+                  {filteredData.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage).map((data) => {
                     return (
                       <tr key={data.id}>
                         {/*corpo tabela*/}         
