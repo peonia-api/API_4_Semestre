0150e19db82a7b5e0447507c0592ab1ff932236a
Jennyads <jennycristina.1998@gmail.com>
2023-06-01 11:17:40 -0300

    adiciona funcionalidade na tela kanbanGroups

--- front/src/pages/kanban/kanbanGroups.tsx
+++ front/src/pages/kanban/kanbanGroups.tsx
@@ -2,65 +2,63 @@ import { Container, Row, Col } from 'react-grid-system';
 import Header from '../../components/Header';
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
+import { GroupsToUser } from '../../types/groupToUser';
+import '../../styles/kanbanGroups.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { URIgroup, URIgroupToUser } from '../../enumerations/uri';
+import { Groups } from '../../types/group';
 
-function kanbanGroups() {
-  
-  const containerStyle: React.CSSProperties = {
-    display: 'flex',
-    justifyContent: 'center',
-    width: '100%',
-  };
-  
-  const colStyle: React.CSSProperties = {
-    backgroundColor: 'green',
-    color: 'white',
-    padding: '90px',
-    margin: '20px',
-    borderRadius: '30px',
-    width: '400%', // Defina a largura desejada aqui, por exemplo, '300px'
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  };
-  
-  const contentStyle: React.CSSProperties = {
-    textAlign: 'center',
-  };
+function KanbanGroups() {
+  const [dataGroup, setGroup] = useState<Groups[]>([]);
+  const [data, setData] = useState<GroupsToUser[]>([]);
+
+  //axios get
+  useEffect(() => {
+    async function fetchGroup() {
+      axios
+        .get(`${URIgroup.PEGAR_GROUP_USER}${localStorage.getItem("userEmail")?.replace(/["]/g, "")}`)
+        .then((response) => {
+          setGroup(response.data);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+
+    fetchGroup();
+
+  }, []);
+
+  console.log(data);
+  console.log(dataGroup);
 
   return (
     <>
-    <Header />
-    <div className='d-flex flex-center flex-column flex-column-fluid hf-spacing px-2 mt-5'>
+      <Header />
+      <div className='d-flex flex-center flex-column flex-column-fluid hf-spacing px-2 mt-5'>
 
-      <div className='container containerback bg-light-opacity rounded mx-auto' style={{ padding: "2rem" }}>
-        <div className="text-center">
-          <h1 className="text-dark mb-0 font-padrao-titulo">
-            Equipes
-          </h1>
+        <div className='container containerback bg-light-opacity rounded mx-auto' style={{ padding: "2rem" }}>
+          <div className="text-center">
+            <h1 className="text-dark mb-0 font-padrao-titulo">
+              Equipes
+            </h1>
+          </div>
+          <div className='containerStyle'>
+            {dataGroup.map((grupo: any) => (
+              <div className='colStyle'>
+                <a href='/kanban'>{grupo.groupName} </a> 
+              </div>
+            ))}
+
+          </div>
+
         </div>
-        <div style={containerStyle}>
-      <Container>
-        <Row>
-          <Col xs={12} sm={6} md={4} lg={3} style={colStyle}>
-            <div style={contentStyle}>Equipe 1</div>
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3} style={colStyle}>
-            <div style={contentStyle}>Equipe 2</div>
-          </Col>
-          <Col xs={12} sm={6} md={4} lg={3} style={colStyle}>
-            <div style={contentStyle}>Equipe 3</div>
-          </Col>
-          {/* Adicione mais Col para mais equipes */}
-        </Row>
-      </Container>
-    </div>
-
       </div>
-    </div>
-      
+
     </>
-  ); 
+  );
 
 }
 
-export default kanbanGroups;
+export default KanbanGroups;
